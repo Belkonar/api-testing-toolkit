7e44522550fd26dbd5e7ff4562f34629935c97c1
Tom <JosephDotson@belkonar.com>
2024-07-01 21:09:34 -0400

    starting workflow

--- src/api_testing_toolkit/__init__.py
+++ src/api_testing_toolkit/__init__.py
@@ -7,14 +7,16 @@ Functions:
 """
 
 from requests.structures import CaseInsensitiveDict
+from requests.models import Response
 from IPython.display import JSON, display as ipy_display
 import json
 
 
-def display(data):
+def display(data, label=None):
     """
     Display the data in a nice way, does not return anything but uses the IPython display function
     :param data: Some (mostly) JSON-able data
+    :param label: Optional label to display
     :return: None
     """
 
@@ -24,10 +26,33 @@ def display(data):
     if isinstance(data, dict):
         data = JSON(data, expanded=True)
 
+    if label:
+        print(label + ':')
+
     ipy_display(data)
+    print('')  # add a new line
 
 
 d = display  # alias for display
+
+
+def display_request(response: Response, headers=False):
+    """
+    Helper function for displaying the response of JSON requests.
+    :param response: The response object
+    :param headers: Whether to display the headers
+    :return: None
+    """
+    if not isinstance(response, Response):
+        print('Not a Response object')
+
+    display(response.json(), 'Data')
+
+    if headers:
+        display(response.headers, 'Headers')
+
+
+dr = display_request  # alias for display_request
 
 
 def load_env(name: str) -> object:
